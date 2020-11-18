b67e325e801d6dc9b465078973e8519bb250dd93
poulstar <info@poulstar.com>
2020-11-18 17:32:07 +0330

    deposite done transaction add...

--- Term 3/ATM/main.py
+++ Term 3/ATM/main.py
@@ -1,10 +1,41 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-from tkinter import Toplevel, messagebox
+from tkinter import messagebox
 import hashlib
 import json
 import datetime
 import random
+
+
+
+
+def deposite():
+    def deposite_money():
+        all = read_json('names.json')
+        if all[index]['balance'] > deposite_amount.get():
+
+            deposite_transaction['username'] = all[index]['username']
+            deposite_transaction['card_number'] = all[index]['card_number']
+            deposite_transaction['amount'] = deposite_amount.get()
+            deposite_transaction['created_at'] = get_datetime()
+            all_tra = read_json('transactions.json')
+
+            all[index]['balance'] -= deposite_amount.get()
+            write_json('names.json', all)
+        else:
+            messagebox.showerror("Less Money", "Not Enough MOney")
+
+    def deposite_destroy():
+        dps.destroy()
+        top.deiconify()
+
+    top.withdraw()
+    dps = tk.Toplevel()
+    tk.Label(dps, text='Amount').grid(row=0, column=0)
+    deposite_amount = tk.IntVar()
+    tk.Entry(dps, textvariable=deposite_amount).grid(row=0, column=1)
+    tk.Button(dps, text='Deposite', command=deposite_money).grid(row=1, column=0, columnspan=2)
+    tk.Button(dps, text='Close', command=deposite_destroy).grid(row=2, column=0, columnspan=2)
 
 
 def get_datetime():
@@ -63,28 +94,37 @@ def register():
 
 
 def find_person(file, username):
+    index = 0
     for person in file:
-            if person['username'] == username:
-                return person
+        if person['username'] == username:
+            return person, index
+        index += 1
     messagebox.showerror("Username Error", "Entered Invalid Username")
     return None
 
 def login():
+    global index
     username = login_user.get() 
     password = to_sha1(login_pass.get())
     file = read_json('names.json')
-    person = find_person(file, username)
+    person, index = find_person(file, username)
     if person is None:
         pass
     else:
         if person["password"] == password:
             top.deiconify()
             root.withdraw()
-
         else:
             messagebox.showerror("Password Error", "Entered Invalid Password")
 
-       
+deposite_transaction = {
+    "username": '',
+    "card_number": 0,
+    "created_at": '',
+    "type": 'deposite',
+    "amount": 0
+}
+
 root = tk.Tk()
 root.title("Bank")
 
@@ -121,7 +161,7 @@ tk.Button(login_form, text="Login", command=login).grid(row=2, column=0, columns
 ###############################################################
 # top level ###################################################
 tk.Button(top, text="Transfer", command=login).grid(row=0, column=0, sticky=tk.W+tk.E)
-tk.Button(top, text="Deposite", command=login).grid(row=0, column=1, sticky=tk.W+tk.E)
+tk.Button(top, text="Deposite", command=deposite).grid(row=0, column=1, sticky=tk.W+tk.E)
 tk.Button(top, text="Balance", command=login).grid(row=1, column=0, sticky=tk.W+tk.E)
 tk.Button(top, text="Change Password", command=login).grid(row=1, column=1, sticky=tk.W+tk.E)
 tk.Button(top, text="EXIT", command=root.destroy).grid(row=3, column=0, columnspan=2, sticky=tk.W+tk.E)
